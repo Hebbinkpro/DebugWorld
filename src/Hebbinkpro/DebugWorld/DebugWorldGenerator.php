3061280fb4c099cd7510a98a66d31d465bdd1943
Hebbinkpro <51849598+hebbinkpro@users.noreply.github.com>
2024-11-14 11:01:15 +0100

    Update api and license

--- src/Hebbinkpro/DebugWorld/DebugWorldGenerator.php
+++ src/Hebbinkpro/DebugWorld/DebugWorldGenerator.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ *   _____       _              __          __        _     _
+ *  |  __ \     | |             \ \        / /       | |   | |
+ *  | |  | | ___| |__  _   _  __ \ \  /\  / /__  _ __| | __| |
+ *  | |  | |/ _ \ '_ \| | | |/ _` \ \/  \/ / _ \| '__| |/ _` |
+ *  | |__| |  __/ |_) | |_| | (_| |\  /\  / (_) | |  | | (_| |
+ *  |_____/ \___|_.__/ \__,_|\__, | \/  \/ \___/|_|  |_|\__,_|
+ *                            __/ |
+ *                           |___/
+ *
+ * Copyright (c) 2024 Hebbinkpro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 namespace Hebbinkpro\DebugWorld;
 
